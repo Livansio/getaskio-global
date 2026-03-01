222c2b0accd2d515565cdeb3e5fdfc65bdf69ea1
Livansio <laschemasc@gmail.com>
2026-03-01 07:45:43 +0500

    Add SEO and Analytics

--- getaskio_new/getaskio-next/app/layout.tsx
+++ getaskio_new/getaskio-next/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -8,7 +9,7 @@ export const metadata: Metadata = {
   openGraph: {
     title: "Askio | Автоматизация бизнеса",
     description: "Семантический брандмауэр между бизнесом и разработкой.",
-    url: "https://getaskio.com",
+    url: "https://www.getaskio.com",
     siteName: "Askio",
     locale: "ru_KZ",
     type: "website",
@@ -26,6 +27,49 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ru">
+      <head>
+        {/* Google Analytics 4 */}
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=G-YRK8817VLP`}
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-YRK8817VLP', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
+
+        {/* Yandex Metrica */}
+        <Script
+          id="yandex-metrica"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js?id=107054693", "ym");
+
+              ym(107054693, "init", {
+                   clickmap:true,
+                   trackLinks:true,
+                   accurateTrackBounce:true,
+                   webvisor:true
+              });
+            `,
+          }}
+        />
+      </head>
       <body>{children}</body>
     </html>
   );
